d75441fb6efb0ea855bcbd5af4899afd1210e712
Gursimrandeep Singh Khela <60434846+Gursimran07316@users.noreply.github.com>
2024-05-22 02:55:25 -0400

    Implemented chat page Ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 
 import "./App.css";
+import ChatScreen from "./Screens/ChatScreen";
 import HomeScreen from './Screens/HomeScreen';
-
+import {   Route, Routes } from "react-router-dom";
 function App() {
   return (
+      
     <div className="App">
-   <HomeScreen/>
+     
+      <Routes>
+      <Route path="/" element={<HomeScreen/>}  />
+      <Route path="/chats" element={<ChatScreen/>} />
+      </Routes>
     </div>
+
+  
   );
 }
 
