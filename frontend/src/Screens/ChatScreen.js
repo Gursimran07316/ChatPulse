8fef392630da59fc877f7d60d9e7a3bfc1195cdc
Gursimrandeep Singh Khela <60434846+Gursimran07316@users.noreply.github.com>
2024-05-23 05:11:01 -0400

    completed frontend and backend logic also implemented sockets

--- frontend/src/Screens/ChatScreen.js
+++ frontend/src/Screens/ChatScreen.js
@@ -1,23 +1,26 @@
-import Chatbox from "../components/ChatBox";
+import { Box } from "@chakra-ui/layout";
+import { useState } from "react";
+
 import MyChats from "../components/MyChats";
+
+
+import { ChatState } from "../Context/ChatProvider";
+import Chatbox from "../components/ChatBox";
 import SideDrawer from "../components/SideDrawer";
 
 const ChatScreen = () => {
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const { user } = ChatState();
+
   return (
     <div style={{ width: "100%" }}>
-      <SideDrawer />
-      <div
-        style={{
-          width: "100%",
-          height: "91.5vh",
-          display: "flex",
-          justifyContent: "space-between",
-          padding: 10,
-        }}
-      >
-        <MyChats />
-        <Chatbox />
-      </div>
+      {user && <SideDrawer />}
+      <Box display="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
     </div>
   );
 };
