fefb085afea91581d27e0dce78df4251e52b694d
Gursimrandeep Singh Khela <60434846+Gursimran07316@users.noreply.github.com>
2024-05-24 00:01:04 -0400

    Bug fixesHome screen app name

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -36,7 +36,7 @@ function Homepage() {
         borderWidth="1px"
       >
         <Text fontSize="4xl" fontFamily="Work sans" textAlign={'center'}>
-          ChartWell
+          ChartPulse
         </Text>
       </Box>
       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
